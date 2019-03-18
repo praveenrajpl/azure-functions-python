1785a7118214757305873b5369657bc5dc3eb713
praveenrajpl <praveen.asu@gmail.com>
2019-03-18 22:20:22 +0000

    testing

---
+++ v2functions/blob-trigger-cosmosdb-out-binding/__init__.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import azure.functions as func
+import http.client, urllib.parse, base64, json
+import requests
+
+subscription_key = os.environ['ComputerVisionSubscription']
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters. All of them are optional.
+    'visualFeatures': 'Description',
+    'language': 'en',
+})
+
+def main(myblob: func.InputStream, doc: func.Out[func.Document]):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes")
+
+    img_data = myblob.read()
+    try:
+        api_url = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?%s" % params
+        print ("API URL:{}".format(api_url))
+        r = requests.post(api_url,
+                    headers=headers,
+                    data=img_data)
+
+        parsed = r.json()
+        logging.info("Response:")
+        logging.info(json.dumps(parsed, sort_keys=True, indent=2))
+
+        # Set output data
+        outdata = {}
+        outdata['name'] = myblob.name
+        taglist = parsed['description']['tags']
+        outdata['text'] =  ' '.join(taglist)
+        logging.info(json.dumps(outdata, sort_keys=True, indent=2))
+
+        ## Store output data using Cosmos DB output binding
+        doc.set(func.Document.from_json(json.dumps(outdata)))
+    except Exception as e:
+        print('Error:')
+        print(e)
